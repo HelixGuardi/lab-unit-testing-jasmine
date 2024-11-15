0a623d4b66d930c189194bf57e5f27a50eee833b
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-11-15 16:36:32 +0100

    iteration 1 - done

--- src/add.js
+++ src/add.js
@@ -1,5 +1,17 @@
 function add (numOne, numTwo) {
-    if (numOne == undefined || numTwo == undefined) {
+    // if (numOne == undefined || numTwo == undefined) {
+    //     return undefined;
+    // }
+
+    // if(typeof numOne !== "number" || typeof numTwo !== "number") {
+    //     return undefined;
+    // }
+
+    // return numOne + numTwo;
+
+    if(numOne === undefined || numTwo === undefined) {
+        return undefined;
+    } else if (typeof numOne !== "number" || typeof numTwo !== "number") {
         return undefined;
     }
 
