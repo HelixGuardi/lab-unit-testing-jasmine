c207f2d95858a56e3e3db0ef5860ed8a1389d952
Victor Hugo Guardiola <victorhugo.pessoal.guardiola@gmail.com>
2024-11-15 17:06:55 +0100

    iteration 2 - done

--- tests/divide.spec.js
+++ tests/divide.spec.js
@@ -1,14 +1,38 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
 
-describe("Iteration 2", () => {
+
+describe("Iteration 2 | Divide", () => {
     describe("Function - divide", () => {
         
         // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
+        it("should be defined", () => {
+            expect(divide).toBeDefined();
+        });
+
+        it("should take two arguments", () => {
+            expect(divide.length).toBe(2);
+        });
+
+        it("should return the division of the two numbers", () => {
+            expect(divide(100, 2)).toEqual(50);
+            expect(divide(25, 5)).toEqual(5);
+            expect(divide(134.2, 4)).toEqual(33.55);
+        });
+
+        it("should return undefined if any of the arguments is not provided", () => {
+            expect(divide(3)).toEqual(undefined);
+            expect(divide()).toEqual(undefined);
+            expect(divide(undefined, 2368)).toEqual(undefined);
+        });
+
+        it("should return NaN if any of the two arguments is not a number", () => {
+            expect(divide(35, "string")).toEqual(NaN);
+            expect(divide(null, 2)).toEqual(NaN);
+            expect(divide(["hello World!"], 347)).toEqual(NaN);
+            expect(divide(true, "patata")).toEqual(NaN);
+        });
 
     })    
 })
 
-*/
